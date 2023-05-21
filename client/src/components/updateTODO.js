59c71da745a856598987851988d92713d9bfa21c
connectrajnish <rajnishresonite@gmail.com>
2023-05-22 04:35:16 +0530

    Modal Css improved

--- client/src/components/updateTODO.js
+++ client/src/components/updateTODO.js
@@ -22,7 +22,8 @@ const updateTODO = ({ handleEditButton, updateId, onCreateTODO }) => {
 
 
     return (
-        <div>
+        <div className="flex justify-center items-center p-2 m-2 overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            
             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex justify-end mr-4 mt-2">
                     <button
@@ -73,7 +74,6 @@ const updateTODO = ({ handleEditButton, updateId, onCreateTODO }) => {
                         </div>
                     </form>
                 </div>
-
             </div>
         </div>
     )
